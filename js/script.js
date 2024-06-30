8c21fe48c0b811360f37842870f2baf683eac8b1
rifatbhuiya567 <rifatbhuiya.personal@gmail.com>
2024-06-30 08:19:42 +0600

    mar-files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+(function() {
+    "use-strict";
+
+    // Window Load
+    $(window).load(function(){
+        $(".btt i").hide();
+    });
+
+    // Window Scroll
+    $(window).scroll(function(){
+        let scrolling = $(this).scrollTop();
+    
+        if( scrolling > 200){
+            $(".btt i").fadeIn(500);
+        }
+        else{
+            $(".btt i").fadeOut(500);
+        }
+    });
+      
+    // On Click BTT
+    $(".btt i").click(function(){
+        $("html, body").animate({
+            scrollTop:0
+        },500)
+    });
+
+    // Dropdown menu
+    $(".dropdown-menu-btn").on("click", function () {
+        $(".dropdown-menu-items").toggle(500);
+        $(".dropdown-menu-btn").toggleClass("dropdown-arrow");
+    });
+})(jQuery);
